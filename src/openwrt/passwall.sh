b1d9d29ca1b5de57800b3983d0ef487f173f0db1
xujiaji <jiajixu@qq.com>
2026-06-15 15:02:54 +0800

    Add diagnostic logging to passwall switch and listener scripts

--- src/openwrt/passwall.sh
+++ src/openwrt/passwall.sh
@@ -2,11 +2,16 @@
 
 action=$1
 
+# 记录开关到底有没有调到本脚本,以及传进来的参数是什么
+logger -t passwall-switch "passwall.sh called with action=[$action]"
+
 if [ "$action" = "on" ];then
+	logger -t passwall-switch "send ubus event: on"
 	ubus send passwall.switch '{"action":"on"}'
 fi
 
 if [ "$action" = "off" ];then
+	logger -t passwall-switch "send ubus event: off"
 	ubus send passwall.switch '{"action":"off"}'
 fi
 
